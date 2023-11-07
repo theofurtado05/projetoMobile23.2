ccc281a10e75608615e2aacbc3df7cf7dde07b28
Amanda Senra <senraamanda16@gmail.com>
2023-11-07 17:44:08 -0300

    Telas de cadastro e esqueci senha

--- app/src/main/java/com/example/telainicial/MainActivity.kt
+++ app/src/main/java/com/example/telainicial/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.telainicial
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,37 +8,47 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
-
-data class UserCredentials(val email: String, val senha: String)
-
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    data class UserCredentials(val email: String, val senha: String)
 
-        val emailEditText = findViewById<EditText>(R.id.emailEditText)
-        val senhaEditText = findViewById<EditText>(R.id.senhaEditText)
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+    class MainActivity : AppCompatActivity() {
+        @SuppressLint("MissingInflatedId")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        val user = UserCredentials("123@123", "123456")
+            val emailEditText = findViewById<EditText>(R.id.emailEditText)
+            val senhaEditText = findViewById<EditText>(R.id.senhaEditText)
+            val loginButton = findViewById<Button>(R.id.loginButton)
+            val resultTextView = findViewById<TextView>(R.id.resultTextView)
 
-        loginButton.setOnClickListener {
-            val inputEmail = emailEditText.text.toString()
-            val inputSenha = senhaEditText.text.toString()
+            val user = UserCredentials("123@123", "123456")
 
-            if (inputEmail == user.email && inputSenha == user.senha) {
-                resultTextView.text = "Login bem-sucedido!"
-                Toast.makeText(this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show()
+            loginButton.setOnClickListener {
+                val inputEmail = emailEditText.text.toString()
+                val inputSenha = senhaEditText.text.toString()
 
-                // Iniciar segundo activity
+                if (inputEmail == user.email && inputSenha == user.senha) {
+                    resultTextView.text = "Login bem-sucedido!"
+                    Toast.makeText(this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show()
+
+                    // Iniciar segundo activity
 //                val intent = Intent(this, SecondActivity::class.java)
 //                startActivity(intent)
-            } else {
-                resultTextView.text = "Email ou senha incorretos. Tente novamente."
-                Toast.makeText(this, "Login falhou. Verifique o email e a senha.", Toast.LENGTH_SHORT).show()
+                } else {
+                    resultTextView.text = "Email ou senha incorretos. Tente novamente."
+                    Toast.makeText(
+                        this,
+                        "Login falhou. Verifique o email e a senha.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
+
     }
 }
