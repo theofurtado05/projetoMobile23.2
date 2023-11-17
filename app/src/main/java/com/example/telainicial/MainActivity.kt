afd7a5fb1449f23555c63d7c37ec41c9e08f53f0
Joao Lucas Curvello <>
2023-11-17 11:59:06 -0300

    sou pica /2

--- app/src/main/java/com/example/telainicial/MainActivity.kt
+++ app/src/main/java/com/example/telainicial/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.telainicial
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,43 +12,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    data class UserCredentials(val email: String, val senha: String)
 
-    class MainActivity : AppCompatActivity() {
-        @SuppressLint("MissingInflatedId")
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val senhaEditText = findViewById<EditText>(R.id.senhaEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val redefinirSenhaTextView = findViewById<TextView>(R.id.redefinir_senha)
+        val registroTextView = findViewById<TextView>(R.id.registro)
 
-            val emailEditText = findViewById<EditText>(R.id.emailEditText)
-            val senhaEditText = findViewById<EditText>(R.id.senhaEditText)
-            val loginButton = findViewById<Button>(R.id.loginButton)
-            val resultTextView = findViewById<TextView>(R.id.resultTextView)
 
-            val user = UserCredentials("123@123", "123456")
+        val user = UserCredentials("123@123", "123456")
 
-            loginButton.setOnClickListener {
-                val inputEmail = emailEditText.text.toString()
-                val inputSenha = senhaEditText.text.toString()
+        loginButton.setOnClickListener {
+            val inputEmail = emailEditText.text.toString()
+            val inputSenha = senhaEditText.text.toString()
 
-                if (inputEmail == user.email && inputSenha == user.senha) {
-                    resultTextView.text = "Login bem-sucedido!"
-                    Toast.makeText(this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show()
-
-                    // Iniciar segundo activity
-//                val intent = Intent(this, SecondActivity::class.java)
-//                startActivity(intent)
-                } else {
-                    resultTextView.text = "Email ou senha incorretos. Tente novamente."
-                    Toast.makeText(
-                        this,
-                        "Login falhou. Verifique o email e a senha.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+            if (inputEmail == user.email && inputSenha == user.senha) {
+                resultTextView.text = "Login bem-sucedido!"
+                Toast.makeText(this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show()
+            } else {
+                resultTextView.text = "Email ou senha incorretos. Tente novamente."
+                Toast.makeText(
+                    this,
+                    "Login falhou. Verifique o email e a senha.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+        registroTextView.setOnClickListener {
+            // Abrir a tela de registro ao clicar no texto "Registre-se"
+            val intent = Intent(this, TelaCadastro::class.java)
+            startActivity(intent)
+        }
 
+        redefinirSenhaTextView.setOnClickListener {
+            // Crie um Intent para iniciar a TelaRedefiniSenhaActivity
+            val intent = Intent(this, TelaRedefiniSenha::class.java)
+
+            // Inicie a nova atividade
+            startActivity(intent)
+        }
     }
+
+    data class UserCredentials(val email: String, val senha: String)
 }
