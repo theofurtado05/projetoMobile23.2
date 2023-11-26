9199b137fd74e4d6d00f38076c4d4ad7c947d90a
Amanda Senra <senraamanda16@gmail.com>
2023-11-26 18:43:29 -0300

    Atualizar Tela

---
+++ app/src/main/java/com/example/telainicial/Tela_config.kt
@@ -0,0 +1,11 @@
+package com.example.telainicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela_config : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_config)
+    }
+}
