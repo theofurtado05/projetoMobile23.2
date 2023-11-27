9410e704fe625e37169671ea83f018006d4a38a7
Amanda Senra <senraamanda16@gmail.com>
2023-11-27 09:30:12 -0300

    Tela Hoje e tela Princípios

---
+++ app/src/main/java/com/example/telainicial/Principioss.kt
@@ -0,0 +1,11 @@
+package com.example.telainicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Principioss : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principioss)
+    }
+}
