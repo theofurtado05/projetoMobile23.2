465911f3e2363917f5fef12424e3180783577278
Amanda Senra <senraamanda16@gmail.com>
2023-11-27 10:25:59 -0300

    Telas oriundas da Hoje

--- app/src/main/java/com/example/telainicial/SaiuNaImprensa.kt
+++ app/src/main/java/com/example/telainicial/SaiuNaImprensa.kt
@@ -3,9 +3,9 @@ package com.example.telainicial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Principios : AppCompatActivity() {
+class SaiuNaImprensa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principioss)
+        setContentView(R.layout.activity_saiu_na_imprensa)
     }
 }
--- app/src/main/java/com/example/telainicial/composicaoAcionaria.kt
+++ app/src/main/java/com/example/telainicial/composicaoAcionaria.kt
@@ -3,9 +3,9 @@ package com.example.telainicial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Principios : AppCompatActivity() {
+class composicaoAcionaria : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principioss)
+        setContentView(R.layout.activity_composicao_acionaria)
     }
 }
