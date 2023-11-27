d135552b0e28089e768bf8dd7d1a6789f23ed1e2
Amanda Senra <senraamanda16@gmail.com>
2023-11-26 22:05:33 -0300

    Tela dados

---
+++ app/src/main/java/com/example/telainicial/Noticias.kt
@@ -0,0 +1,11 @@
+package com.example.telainicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Noticias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_noticias)
+    }
+}
