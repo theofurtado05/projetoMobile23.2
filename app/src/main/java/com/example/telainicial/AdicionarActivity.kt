e9c51ce73baadea1e78847b816c253d9150ec42e
Joao Lucas Curvello <103194851+JukasDEV@users.noreply.github.com>
2023-11-28 14:15:57 -0300

    Producao

--- app/src/main/java/com/example/telainicial/AdicionarActivity.kt
+++ app/src/main/java/com/example/telainicial/AdicionarActivity.kt
@@ -1,12 +1,30 @@
 package com.example.telainicial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.telainicial.R
 
 class AdicionarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.adicionar_reuniao)
+
+        val Voltar: ImageView = findViewById(R.id.botao_voltar)
+
+        val Button2: Button = findViewById(R.id.button2)
+
+
+        Button2.setOnClickListener {
+            val intent = Intent(this, Reuniao::class.java)
+            startActivity(intent)
+        }
+
+        Voltar.setOnClickListener {
+            val intent = Intent(this, Reuniao::class.java)
+            startActivity(intent)
+        }
     }
 }
